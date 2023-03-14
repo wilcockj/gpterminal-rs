aeb71a6064cf003cd3fcb85d34abf7b36396820e
James Wilcock <jameswilcock8@gmail.com>
2023-03-14 12:49:37 -0700

    got chat example working with markdown rendering

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use dotenvy::dotenv;
+use openai::{
+    chat::{ChatCompletion, ChatCompletionMessage, ChatCompletionMessageRole},
+    models::ModelID,
+};
+use std::io::{stdin, stdout, Write};
+use termimad::crossterm::{execute, style::Color::*, terminal};
+use termimad::*;
+
+#[tokio::main]
+async fn main() {
+    // Make sure you have a file named `.env` with the `OPENAI_KEY` environment variable defined!
+    dotenv().unwrap();
+
+    let mut messages = vec![ChatCompletionMessage {
+        role: ChatCompletionMessageRole::System,
+        content: "You are a large language built into a command line interface as an example of what the `openai` Rust library made by Valentine Briese can do.".to_string(),
+        name: None,
+    }];
+
+    let mut skin = MadSkin::default();
+    skin.set_headers_fg(rgb(255, 187, 0));
+    skin.bold.set_fg(Yellow);
+    skin.italic.set_fgbg(Magenta, rgb(30, 30, 40));
+    skin.bullet = StyledChar::from_fg_char(Yellow, '⟡');
+    skin.quote_mark.set_fg(Yellow);
+    loop {
+        print!("User: ");
+        stdout().flush().unwrap();
+
+        let mut user_message_content = String::new();
+
+        stdin().read_line(&mut user_message_content).unwrap();
+        messages.push(ChatCompletionMessage {
+            role: ChatCompletionMessageRole::User,
+            content: user_message_content,
+            name: None,
+        });
+
+        let chat_completion = ChatCompletion::builder(ModelID::Gpt3_5Turbo, messages.clone())
+            .create()
+            .await
+            .unwrap()
+            .unwrap();
+        let returned_message = chat_completion.choices.first().unwrap().message.clone();
+
+        let combined_response = format!(
+            "{:#?}: {}",
+            &returned_message.role,
+            &returned_message.content.trim()
+        );
+        skin.print_text(&combined_response);
+        messages.push(returned_message);
+    }
+}
